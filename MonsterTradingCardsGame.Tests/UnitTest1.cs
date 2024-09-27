41458fd4eacbef5a4b5229f51375e2da395f88e6
Velid Heldic <velid2004@gmail.com>
2024-09-27 22:12:37 +0200

    Created project

---
+++ MonsterTradingCardsGame.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MonsterTradingCardsGame.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
